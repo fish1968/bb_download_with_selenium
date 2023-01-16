d1b49deae8f367a5e7921184f47be43fd3130957
fish1968 <yabincheng@link.cuhk.edu.cn>
2023-01-17 01:02:10 +0800

    Fix a typo in info.py

--- info.py
+++ info.py
@@ -2,6 +2,6 @@ S_ID = "" # Your Student ID
 password = "" 
 
 if S_ID == "":
-    S_ID = input("Input your name:")
+    S_ID = input("Input your student ID:")
 if password == "":
     password = input("Input your password")
